6387464927c3c5154d7150a587c96eaa19c16b34
ESTUDIANTES <ESTUDIANTES@10.3.2.219>
2019-09-12 16:49:33 -0500

    comentando la rama david
    esto es un commit

---
+++ test/src/main/java/uan/edu/co/test/Main.java
@@ -0,0 +1,17 @@
+package uan.edu.co.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hola mundo" );
+        
+        System.out.println("Esta es una modificación hecha por David ");
+        Integer a = 2;
+        Integer b = 3;
+        
+        System.out.println("Suma " + (a+b));
+
+	}
+
+}
