2e6a0383bd61ce6052ceda13a117ce67bcc6e807
ESTUDIANTES <ESTUDIANTES@10.3.2.219>
2019-09-12 16:43:35 -0500

    Modificación David
    modificado método main

---
+++ test/src/main/java/uan/edu/co/test/App.java
@@ -0,0 +1,19 @@
+package uan.edu.co.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola mundo" );
+        
+        System.out.println("Esta es una modificación hecha por David");
+        Integer a = 0;
+        Integer b = 3;
+        
+        System.out.println("Suma " + (a+b));
+    }
+}
